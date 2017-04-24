f38bc61e429558142a41d0930842ca7a6f4dbd78
L-Dogg <szymek94sul@gmail.com>
2017-04-24 13:43:35 +0200

    Fixes to controller code

--- WarsawNaviHelper/app/src/main/java/pl/kuc_industries/warsawnavihelper/ZTM/Query/TramBusAPI.java
+++ WarsawNaviHelper/app/src/main/java/pl/kuc_industries/warsawnavihelper/ZTM/Query/TramBusAPI.java
@@ -1,13 +1,13 @@
 package pl.kuc_industries.warsawnavihelper.ZTM.Query;
 
-import pl.kuc_industries.warsawnavihelper.ZTM.QueryResult.TramBusQueryResult;
+import pl.kuc_industries.warsawnavihelper.ZTM.QueryResult.TramBus;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface TramBusAPI {
     @GET("busestrams_get/")
-    Call<TramBusQueryResult> getTrams(
+    Call<TramBus> getTrams(
             @Query("resource_id") String resource_id,
             @Query("apikey") String apikey,
             @Query("type") int type,
--- WarsawNaviHelper/app/src/main/java/pl/kuc_industries/warsawnavihelper/ZTM/ZTMController.java
+++ WarsawNaviHelper/app/src/main/java/pl/kuc_industries/warsawnavihelper/ZTM/ZTMController.java
@@ -9,6 +9,7 @@ import com.google.gson.GsonBuilder;
 
 import pl.kuc_industries.warsawnavihelper.Constants;
 import pl.kuc_industries.warsawnavihelper.ZTM.Query.TramBusAPI;
+import pl.kuc_industries.warsawnavihelper.ZTM.QueryResult.TramBus;
 import pl.kuc_industries.warsawnavihelper.ZTM.QueryResult.TramBusQueryResult;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -16,7 +17,7 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
-public class ZTMController implements Callback<TramBusQueryResult> {
+public class ZTMController implements Callback<TramBus> {
     private static final String TAG = "ZTMController";
 
     private static final String BASE_URL = "https://api.um.warszawa.pl/api/action/";
@@ -33,7 +34,7 @@ public class ZTMController implements Callback<TramBusQueryResult> {
 
         TramBusAPI tramBusAPI = retrofit.create(TramBusAPI.class);
 
-        Call<TramBusQueryResult> call = tramBusAPI.getTrams(
+        Call<TramBus> call = tramBusAPI.getTrams(
                 RESOURCE_ID,
                 Constants.UM_API_KEY,
                 1,
@@ -44,19 +45,18 @@ public class ZTMController implements Callback<TramBusQueryResult> {
     }
 
     @Override
-    public void onResponse(Call<TramBusQueryResult> call, Response<TramBusQueryResult> response) {
+    public void onResponse(Call<TramBus> call, Response<TramBus> response) {
         if(response.isSuccessful()) {
-            TramBusQueryResult result = response.body();
+            TramBus result = response.body();
             Log.wtf(TAG, response.toString());
-            Log.wtf(TAG, "LAT: " + result.getLat() + " LON" + result.getLon());
-            Log.wtf(TAG, "Time: " + result.getTime());
+            Log.wtf(TAG, "GPS call time: " + result.getResult().get(0).getTime());
         } else {
             System.out.println(response.errorBody());
         }
     }
 
     @Override
-    public void onFailure(Call<TramBusQueryResult> call, Throwable t) {
+    public void onFailure(Call<TramBus> call, Throwable t) {
         t.printStackTrace();
         Log.wtf(TAG, t.getStackTrace().toString());
     }
